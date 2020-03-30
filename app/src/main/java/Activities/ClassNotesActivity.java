ac9fe4a8fbf6cf78c753313c2c15a90f7076bb72
rajsingha <r.singha4520@gmail.com>
2020-03-30 21:12:08 +0530

    Initial Commit

---
+++ app/src/main/java/Activities/ClassNotesActivity.java
@@ -0,0 +1,13 @@
+package Activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClassNotesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/lms/android/app/DrawerModel/DashboardActvity.java
+++ app/src/main/java/com/lms/android/app/DrawerModel/DashboardActvity.java
@@ -1,8 +1,11 @@
 package com.lms.android.app.DrawerModel;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -30,6 +33,10 @@ public class DashboardActvity extends AppCompatActivity implements NavigationVie
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
 
         drawerLayout =findViewById(R.id.dashboard_drawerLayout);
         navigationView = findViewById(R.id.nav_view);
@@ -48,14 +55,14 @@ public class DashboardActvity extends AppCompatActivity implements NavigationVie
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
-        customLoadingDialog.startLoadingDialog();
+        /*customLoadingDialog.startLoadingDialog();
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 customLoadingDialog.dismissLoadingDialog();
             }
-        },10000);
+        },10000);*/
 
 
 
